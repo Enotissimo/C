8c324b7b12ce31ce277040e07ec6889dd707338d
IMPERIUMOFRACOONKIND <dobr9k@internet.ru>
2024-04-14 12:34:44 +0300

    Add terminator file

---
+++ terminator.cpp
@@ -0,0 +1,12 @@
+/*Исправьте предложенную задачу так, чтобы она выводила следующий текст:
+
+
+"I'll be back!" 
+	(c)Terminator 
+ */
+
+#include <stdio.h>
+int main(void){
+  printf("\"I'll be back!\"\n\t(c)Terminator");
+  return 0;
+}
