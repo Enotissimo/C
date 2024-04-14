5170cd567d4a03938b35f8d17416bfb753e7371f
IMPERIUMOFRACOONKIND <dobr9k@internet.ru>
2024-04-14 12:25:11 +0300

    Add wakeupneo file

---
+++ wakeupneo.cpp
@@ -0,0 +1,16 @@
+/*Вы смотрели фильм «Матрица»? Если нет, то советую посмотреть. 
+ * А задание будет следующим: переписать программу «Hello, world!» так, 
+ * чтобы на экране появился следующий текст:
+
+Wake up, Neo...
+
+The Matrix has you...
+
+Follow the white rabbit...*/
+
+#include <stdio.h>
+
+int main() {
+  printf ("Wake up, Neo...\n\nThe Matrix has you...\n\nFollow the white rabbit...");
+  return 0;
+}
