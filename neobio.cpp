1b2f5b2dad7233b336d050e9c710fed0a79c8c35
IMPERIUMOFRACOONKIND <dobr9k@internet.ru>
2024-04-14 12:27:16 +0300

    Add neobio file

---
+++ neobio.cpp
@@ -0,0 +1,15 @@
+/*Напишите программу, которая выводит на экран следующие строки:
+
+NICKNAME: Neo
+CITY: St-Petersburg
+AGE: 35
+HEIGHT: 180
+WEIGHT: 75
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf ("NICKNAME: Neo\nCITY: St-Petersburg\nAGE: 35\nHEIGHT: 180\nWEIGHT: 75");
+  return 0;
+}
